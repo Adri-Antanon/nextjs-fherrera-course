8294b593b734eef309ae2382619a432f27a85bd7
Adrian Antanon Orozco <aantanon@brothapps.com>
2022-03-07 00:38:45 +0100

    feat: add different next/head components

--- src/data/branches.js
+++
@@ -1,8 +0,0 @@
-export const listOfBranches = [
-  {
-    title: 'Introducción a Next.js',
-    branch: '1-introduction',
-    description:
-      'Explicación y visualizado general de cómo funciona Next, utilización de componentes propios como Link o head, API generada por defecto, Layouts, CSS Module...',
-  },
-];
