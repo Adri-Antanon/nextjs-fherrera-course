5ae3f44adc1c0e31c9c24b1593fb3200110be04e
Adrian Antanon Orozco <aantanon@brothapps.com>
2022-03-08 18:06:13 +0100

    refactor: convert api files to ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import { NextPage } from 'next';
-import '../styles/globals.css';
 import { ReactElement, ReactNode } from 'react';
 import { AppProps } from 'next/app';
+import { NextPage } from 'next';
+
+import '../styles/globals.css';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
