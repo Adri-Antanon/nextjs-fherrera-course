1e8768b929b294374e01138efdddb3dead15a892
Adrian Antanon Orozco <aantanon@brothapps.com>
2022-03-10 01:05:11 +0100

    feat: add nextui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from 'next/app';
+import { NextUIProvider } from '@nextui-org/react';
 
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import { Button } from '@nextui-org/react';
 import { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Hola mundo</h1>
+      <Button color={'gradient'}>Hola mundo</Button>
     </>
   );
 };
